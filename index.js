b82873838bdf278ccec5f89abeb36701d512fc9b
devendra-alt <devendramulewa171@gmail.com>
2023-05-22 18:52:25 +0530

    Added mobile humburger

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
-
-
-const menu_trigger = () => {
-
-};
+const menuBtn = document.getElementById("menu-i");
+const closeBtn = document.getElementById("menu-i-close");
+const mobileMenu = document.querySelector("#mobile-nav");
+const menuItems = document.querySelectorAll("#mobile-nav ul li");
+const openMenu = () => mobileMenu.classList.add("open-side-menu");
+const closeMenu = () => mobileMenu.classList.remove("open-side-menu");
+menuItems.forEach((el) => {
+  el.addEventListener("click", () => closeMenu());
+});
+menuBtn.addEventListener("click", () => openMenu());
+closeBtn.addEventListener("click", () => closeMenu());
