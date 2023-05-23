5e03a2d096276d277a97382daed97ccf4acc8ff0
devendra-alt <devendramulewa171@gmail.com>
2023-05-23 12:18:11 +0530

    Added seeProject btn in index.js createProject

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-//project section
-const projects_el = document.querySelector('#projects');
+// project section
+
+const projectsEl = document.querySelector('#projects');
 
 // mobile menu
+
 const mobileMenu = document.querySelector('.mobile-nav');
 const menuItems = document.querySelectorAll('.mobile-nav ul li');
 const menuBtn = document.getElementById('menu-i');
 const closeBtn = document.getElementById('menu-i-close');
 
 // mobile menu
+
 menuItems.forEach((el) => {
   el.addEventListener('click', () => mobileMenu.classList.toggle('open-menu'));
 });
@@ -18,47 +21,82 @@ closeBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('open-menu');
 });
 
-//project section
-const createProject = (project) => {
-  let project_el = document.createElement('article');
-  project_el.id = project.id;
-  project_el.className = 'project';
-  let project_img = document.createElement('img');
-  project_img.src = `assets/images/${project.featured_images[0]}.svg`;
-  project_img.className = 'project-img';
-  project_img.alt = 'project image';
-  let project_overlay_el = document.createElement('div');
-  project_overlay_el.classList.add('project-overlay', 'flex');
-  let project_name_el = document.createElement('p');
-  project_name_el.textContent = `${project.name}`;
-  let ul_el = document.createElement('ul');
-  ul_el.className = 'project-langs';
-  for (let i = 0; i < project.technologies.length; i++) {
-    const li_el = document.createElement('li');
-    li_el.className = 'project-lang';
-    li_el.textContent = `${project.technologies[i]}`;
-    ul_el.appendChild(li_el);
-  }
-  project_overlay_el.appendChild(project_name_el);
-  project_overlay_el.appendChild(ul_el);
-  project_el.appendChild(project_img);
-  project_el.appendChild(project_overlay_el);
+// project section
 
-  return project_el;
+const createProjectBtn = () => {
+  const projectBtnEl = document.createElement('button');
+  const spanEl = document.createElement('span');
+  const imgEl = document.createElement('img');
+
+  projectBtnEl.classList.add('project-btn', 'flex');
+  spanEl.textContent = 'See this project';
+
+  imgEl.src = 'assets/images/right-arrow.svg';
+  imgEl.height = 15;
+  imgEl.width = 20;
+
+  imgEl.alt = 'project btn img';
+
+  projectBtnEl.appendChild(spanEl);
+  projectBtnEl.appendChild(imgEl);
+
+  return projectBtnEl;
+};
+
+const createProjectImage = (project) => {
+  const projectImg = document.createElement('img');
+  projectImg.src = `assets/images/${project.featured_images[0]}.svg`;
+  projectImg.className = 'project-img';
+  projectImg.alt = 'project image';
+  return projectImg;
+};
+
+const createProjectTechStack = (project) => {
+  const ulEl = document.createElement('ul');
+  ulEl.className = 'project-langs';
+  for (let i = 0; i < project.technologies.length; i += 1) {
+    const liEl = document.createElement('li');
+    liEl.className = 'project-lang';
+    liEl.textContent = `${project.technologies[i]}`;
+    ulEl.appendChild(liEl);
+  }
+  return ulEl;
+};
+
+const createProjectOverlay = (project) => {
+  const projectOverlayEl = document.createElement('div');
+  projectOverlayEl.classList.add('project-overlay', 'flex');
+  const projectNameEl = document.createElement('p');
+  projectNameEl.textContent = `${project.name}`;
+
+  projectOverlayEl.appendChild(projectNameEl);
+  projectOverlayEl.appendChild(createProjectTechStack(project));
+  projectOverlayEl.appendChild(createProjectBtn());
+
+  return projectOverlayEl;
+};
+
+const createProject = (project) => {
+  const projectEl = document.createElement('article');
+  projectEl.id = project.id;
+  projectEl.className = 'project';
+
+  projectEl.appendChild(createProjectImage(project));
+  projectEl.appendChild(createProjectOverlay(project));
+  return projectEl;
 };
 
 const loadProjects = () => {
-  const projects_el = document.querySelector('#projects');
   fetch('projects.json')
     .then((response) => response.json())
     .then((data) => {
       const projectArray = data.projects;
       projectArray.forEach((project) => {
-        projects_el.appendChild(createProject(project));
+        projectsEl.appendChild(createProject(project));
       });
     })
-    .catch((error) => {
-      console.error('Error:', error);
+    .catch(() => {
+      // console.error('Error:', error);
     });
 };
 
